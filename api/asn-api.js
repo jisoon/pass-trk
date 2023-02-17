8919ba9d35770a92e4a90b8fe34de0b84435cd4c
jisoo <jisoon0923@gmail.com>
2023-02-17 13:50:44 +0900

    불필요 파일 삭제

--- api/asn-api.js
+++ api/asn-api.js
@@ -1,22 +1,47 @@
 const express = require("express");
+const { body, validationResult } = require('express-validator');
 const router = express.Router();
 const asnService = require('../service/asn-service');
 
-router.post('/', async (req, res) => {
+router.post('/',
+    [
+      body('msg_type').notEmpty().withMessage(
+          'Message check parameter fail to pass(msg_type)'),
+      body('msg_id').notEmpty().withMessage(
+          'Message check parameter fail to pass(msg_id)'),
+      body('from_code').notEmpty().withMessage(
+          'Message check parameter fail to pass(from_code)'),
+      body('partner_code').notEmpty().withMessage(
+          'Message check parameter fail to pass(partner_code)'),
+      body('data_digest').notEmpty().withMessage(
+          'Message check parameter fail to pass(data_digest)'),
+      body('logistics_interface').notEmpty().withMessage(
+          'Message check parameter fail to pass(logistics_interface)'),
 
-  const refineReq = {
-    msg_type: req.body.msg_type,
-    msg_id: req.body.msg_id,
-    from_code: req.body.from_code,
-    partner_code: req.body.partner_code,
-    data_digest: req.body.data_digest,
-    logistics_interface: req.body.logistics_interface,
-  }
+    ], async (req, res) => {
 
-  await asnService.add(refineReq);
+      const errors = validationResult(req);
 
-  res.json({success: true});
-});
+      if (!errors.isEmpty()) {
+        return res.status(200).json({
+          success: "false",
+          errorCode: "S20",
+          errors: errors.array()[0].msg});
+      }
+
+      const refineReq = {
+        msg_type: req.body.msg_type,
+        msg_id: req.body.msg_id,
+        from_code: req.body.from_code,
+        partner_code: req.body.partner_code,
+        data_digest: req.body.data_digest,
+        logistics_interface: req.body.logistics_interface,
+      }
+
+      await asnService.add(refineReq);
+
+      res.json({success: "true"});
+    });
 
 function getIp(req) {
   let ips = (
--- api/asn2-api.js
+++ api/asn2-api.js
@@ -1,25 +1,50 @@
 const express = require("express");
 const router = express.Router();
+const { body, validationResult } = require('express-validator');
 const asn2Service = require('../service/asn2-service');
 
-router.post('/', async (req, res) => {
-  const returnParcel = req.body.returnParcel;
-  const refineReq = {
-    logisticsOrderCode: req.body.logisticsOrderCode,
-    trackingNumber: req.body.trackingNumber,
-    bizType: req.body.bizType,
-    logisticsOrderCreateTime: req.body.logisticsOrderCreateTime,
-    returnParcel_name: returnParcel.name,
-    returnParcel_phone: returnParcel.phone,
-    returnParcel_mobile: returnParcel.mobile,
-    laneCode: req.body.laneCode,
-    undeliverableOption: req.body.undeliverableOption,
-  }
+router.post('/', [
+      body('logisticsOrderCode').notEmpty().withMessage(
+          'Message check parameter fail to pass(logisticsOrderCode)'),
+      body('trackingNumber').notEmpty().withMessage(
+          'Message check parameter fail to pass(trackingNumber)'),
+      body('bizType').notEmpty().withMessage(
+          'Message check parameter fail to pass(bizType)'),
+      body('logisticsOrderCreateTime').notEmpty().withMessage(
+          'Message check parameter fail to pass(logisticsOrderCreateTime)'),
+      body('returnParcel').notEmpty().withMessage(
+          'Message check parameter fail to pass(returnParcel)'),
+      body('laneCode').notEmpty().withMessage(
+          'Message check parameter fail to pass(laneCode)'),
+      body('undeliverableOption').notEmpty().withMessage(
+          'Message check parameter fail to pass(undeliverableOption)'),
+    ],
+    async (req, res) => {
+      const returnParcel = req.body.returnParcel;
+      const refineReq = {
+        logisticsOrderCode: req.body.logisticsOrderCode,
+        trackingNumber: req.body.trackingNumber,
+        bizType: req.body.bizType,
+        logisticsOrderCreateTime: req.body.logisticsOrderCreateTime,
+        returnParcel_name: returnParcel.name,
+        returnParcel_phone: returnParcel.phone,
+        returnParcel_mobile: returnParcel.mobile,
+        laneCode: req.body.laneCode,
+        undeliverableOption: req.body.undeliverableOption,
+      }
+      const errors = validationResult(req);
+      if (!errors.isEmpty()) {
+        return res.status(200).json({
+          success: "false",
+          errorCode: "S20",
+          errors: errors.array()[0].msg});
+      }
 
-  await asn2Service.add(refineReq);
 
-  res.json({success: true});
-});
+      await asn2Service.add(refineReq);
+
+      res.json({success: "true"});
+    });
 
 function getIp(req) {
   let ips = (
