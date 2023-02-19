40cbb9b832546d48f137c56808bac0445f81a676
jisoo <jisoon0923@gmail.com>
2023-02-19 22:44:59 +0900

    validation 체크 추가

--- api/trk-api.js
+++ api/trk-api.js
@@ -1,20 +1,52 @@
 const express = require("express");
 const router = express.Router();
 const trackingService = require('../service/tracking-service');
+const {query, validationResult} = require("express-validator");
+const xml = require('xml');
 
-router.get('/', async (req, res) => {
+router.get('/'
+    , [query('comp').notEmpty().withMessage(
+        'Message check parameter fail to pass(comp)')
+      , query('trkcd').notEmpty().withMessage(
+          'Message check parameter fail to pass(trkcd)')
+      , query('trkno').notEmpty().withMessage(
+          'Message check parameter fail to pass(trkno)')
+    ]
+    , async (req, res) => {
 
-  const refineReq = {
-    ip : getIp(req),
-    comp : req.query.comp,
-    trkcd : req.query.trkcd,
-    trkno : req.query.trkno,
-  }
+      const errors = validationResult(req);
 
-  const refineResult = await trackingService.tracking(refineReq);
+      if (!errors.isEmpty()) {
+        if (req.query.trkcd && req.query.trkcd.toUpperCase() === 'POST') {
+          const errorData = {
+            error: [
+              {errorCode: "S20"},
+              {errors: errors.array()[0].msg},
+            ]
+          }
+          res.set('Content-Type', 'application/xml');
+          return res.send(xml(errorData));
+        } else {
+          return res.status(200).json({
+            success: "false",
+            errorCode: "S20",
+            errors: errors.array()[0].msg
+          });
+        }
+      }
 
-  res.json(refineResult);
-});
+      const refineReq = {
+        ip: getIp(req),
+        comp: req.query.comp,
+        trkcd: req.query.trkcd,
+        trkno: req.query.trkno,
+      }
+
+      const refineResult = await trackingService.tracking(refineReq);
+
+      res.json(refineResult);
+    })
+;
 
 function getIp(req) {
   let ips = (
